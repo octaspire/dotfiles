e67dc942e5973b31a2626416e9ce572a35f914e9
octaspire <octaspire@octaspire.com>
2018-04-02 17:48:51 +0300

    Update Emacs and fish configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,3 +6,16 @@ set PATH ~/bin $PATH
 
 set -x GPG_TTY (tty)
 
+switch (uname)
+       case Darwin
+       # Add coreutils (for example sha512sum) into the PATH and MANPATH.
+       # Coreutils can be installed on macOS with
+       # brew install coreutils
+       set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+       set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+end
+
+# Load private settings from a separate file, if it is present.
+if test -e  ~/.secrets/config/fish/config.fish
+   source ~/.secrets/config/fish/config.fish
+end
