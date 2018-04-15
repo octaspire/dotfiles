151fe0930e4f594a547191aec561434ed95103bb
octaspire <octaspire@octaspire.com>
2018-04-15 08:32:49 +0300

    Update Emacs configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,11 +1,31 @@
 (require 'package)
-(setq package-enable-at-startup nil)
+
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("org"   . "https://orgmode.org/elpa/"))
+
 (package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(use-package org
+  :ensure t
+  :mode (("\\.org$" . org-mode)))
+
+;(unless (package-installed-p 'auto-package-update)
+;  (package-refresh-contents)
+;  (package-install 'auto-package-update))
+
+;(package-install 'org)
+;(package-initialize)
+; Org mode cannot probably be updated from myinit.org
+; because org mode would be active during the update.
+;(auto-package-update-now)
 
 (org-babel-load-file (locate-user-emacs-file "myinit.org"))
