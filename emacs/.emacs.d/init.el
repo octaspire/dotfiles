70434475b4729ab34bf20e67fcce031984147607
octaspire <octaspire@octaspire.com>
2018-05-27 14:17:49 +0300

    Use package rainbow-delimiters, change theme and line hl color in Emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,14 +18,17 @@
   :ensure t
   :mode (("\\.org$" . org-mode)))
 
-;(unless (package-installed-p 'auto-package-update)
-;  (package-refresh-contents)
-;  (package-install 'auto-package-update))
-
-;(package-install 'org)
-;(package-initialize)
-; Org mode cannot probably be updated from myinit.org
-; because org mode would be active during the update.
-;(auto-package-update-now)
-
 (org-babel-load-file (locate-user-emacs-file "myinit.org"))
+
+(custom-set-faces
+;;Rainbow delimiters font modification is based on
+;;https://ericscrivner.me/2015/06/better-emacs-rainbow-delimiters-color-scheme/
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
+
