b7f7acb01015c3c9c8d79b5da35fe74e1278dbb8
octaspire <octaspire@octaspire.com>
2018-08-12 21:07:34 +0300

    Improve rainbow delimiter colors on light background

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,12 +23,12 @@
 (custom-set-faces
 ;;Rainbow delimiters font modification is based on
 ;;https://ericscrivner.me/2015/06/better-emacs-rainbow-delimiters-color-scheme/
- '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark red"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "dark green"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "dark blue"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "hot pink"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "turquoise1"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "magenta4"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "tomato4"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "LightSteelBlue4")))))
 
